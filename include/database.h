df01e3ea0d812df63566c81541fbf097f12bfbc3
R0n1z3 <111209017+R0n1z3@users.noreply.github.com>
2026-08-05 23:58:07 -0400

    Adding database core functions.

--- include/database.h
+++ include/database.h
@@ -1,3 +1,7 @@
-void openConnection();
+#include <iostream>
+#include <string>
+#include <sqlite3.h>
+
+bool openConnection(sqlite3** db, const std::string& dbPath);
 void closeConnection();
 void schemaInit();
--- src/database.cpp
+++ src/database.cpp
@@ -1,1 +1,27 @@
 #include "database.h"
+
+
+bool openConnection(sqlite3** db, const std::string& dbPath) {
+    
+    sqlite3* myDb = nullptr; // This is the pointer to the database connection handle.
+    sqlite3* errmsg = nullptr; // This pointer is for the error message that gets returned if the database connection fails.
+    sqlite3* closeCode = nullptr; // This pointer is for the close connection status. 
+    int rc = sqlite3_open(u8"dbPath", &myDb); // RC holds the status of the connection to the database.
+
+    if(rc != SQLITE_OK) {
+
+        std::cout << "Connection Failed: " << sqlite3_errmsg(errmsg) << " " << std::endl;
+        int cc = sqlite3_close(closeCode);
+
+        if(cc != SQLITE_OK) {
+            std::cout << "Connection closed! " << std::endl;
+            return false;
+        }
+    }
+    
+    
+
+
+
+
+}
